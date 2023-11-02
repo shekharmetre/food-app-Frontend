1cbe33018d38cba19f3e8a996cc3375d8f32167f
Kedarnath <Kalbhairav307@gmail.com>
2023-11-02 11:12:13 +0530

    deploy-error fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Server options go here
+    port: 3000, // Specify the port number
+    open: true, // Automatically open the default browser when the server starts
+  },
 })
